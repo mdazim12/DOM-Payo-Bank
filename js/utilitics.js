292267ceebda99c601a65667f946cdaa26bd1da2
Azim <azimuddin807@gmail.com>
2024-09-19 23:07:36 +0600

    reusbale function

---
+++ js/utilitics.js
@@ -0,0 +1,10 @@
+
+    // function getInputFieldValueById(){
+    //     const addMoney = document.getElementById('input-add-money').value;
+    //     return addMoney;
+    // }
+
+    function getInputFieldValueById(id){
+        const inputValue = document.getElementById(id).value;
+        return inputValue;
+    }
