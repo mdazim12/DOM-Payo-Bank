b4c8eb5bd62f54cd4796d0a0a4f4cb3d427bf117
Azim <azimuddin807@gmail.com>
2024-09-19 23:55:19 +0600

    cashout using function?

--- js/cashOut.js
+++ js/cashOut.js
@@ -3,5 +3,15 @@
         event.preventDefault();
         const cashOut = getInputFieldValueById('input-cash-out');
         const pinNumber = getInputFieldValueById('input-cashout-pin');
-        console.log('cashout button clicked', cashOut, pinNumber);
+       
+        if(pinNumber === 1234){
+            const balance = getTextFieldById('account-balance');
+            const newBalance = balance - cashOut;
+
+            document.getElementById('account-balance').innerText = newBalance;
+        }
+        else{
+            alert('Input Correct PIN');
+        }
+
     })
